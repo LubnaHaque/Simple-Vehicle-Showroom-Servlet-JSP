f54a79dc7c2c805d4c1715906c7699fae3df22e3
Lubna Haque <lubu7035@gmail.com>
2020-09-25 12:38:59 +0600

    added DB, update readme file

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Simple-Vehicle-Showroom-Servlet-JSP
-users can just add,remove and see the vehicle list
+users can just add, remove, and see the vehicle list.
+I have given the database within the vehicleDB folder.
+-registration: username and password filter(if the length is less than 3), password hash(MD5).
+-login
+-add vehicle: you can add your vehicle by giving some info
+-remove vehicle: remove vehicle by giving model number(if forget then you can see your list) 
+-it shows only your vehicles.
+
+(In the console, it shows-registration failed or success, login success or failed, removed vehicle, add vehicle, any exception message, etc) 
---
+++ vehicleDB/vehicle_users.sql
@@ -0,0 +1,54 @@
+-- MySQL dump 10.13  Distrib 5.7.31, for Linux (x86_64)
+--
+-- Host: localhost    Database: vehicle
+-- ------------------------------------------------------
+-- Server version	5.7.31-0ubuntu0.16.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `show_room_id` varchar(15) NOT NULL,
+  `full_name` varchar(15) DEFAULT NULL,
+  `user_name` varchar(15) NOT NULL,
+  `password` varchar(40) DEFAULT NULL,
+  PRIMARY KEY (`show_room_id`),
+  UNIQUE KEY `user_name` (`user_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES ('1234','Lubna Haque','nohai','4ba8fa1f5f5501568e8ec4f2e768ae80'),('32','Sharmin','nupur','60b07e5d7364233ec0b96fdcd62c158c'),('asde','Imrarul Haque','shikhon','3cac25802565d81f6af333e87ed8b767'),('asw3','Sadia Jannat','shama','a3e1576753da5c8c18d4ddf3813a0d01'),('fdfd','Imdadul','likhon','0eda6e9cdfb9fbd2c04bd06b5101471c');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-09-25 12:23:26
